c204cbe73aaa63cb8b36c4b6d9c2067de97442a0
eriny1 <erenynader734@gmail.com>
2024-11-20 15:17:17 -0800

    blocking movement
    i added blocking movement to my world

--- 2DBeginnerAdventureWasefEriny/Assets/Scripts/PlayerController.cs
+++ 2DBeginnerAdventureWasefEriny/Assets/Scripts/PlayerController.cs
@@ -4,23 +4,30 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    Rigidbody2D Rigidbody2D;
+    float horizontal;
+    float vertical;
     // Start is called before the first frame update
     void Start()
     {
-       // QualitySettings.vSyncCount = 0;
+        // QualitySettings.vSyncCount = 0;
         //Application.targetFrameRate = 10;
-
+        Rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-
+         horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+    }
+    private void FixedUpdate()
+    {
         Vector2 position = transform.position;
         position.x = position.x + 5.0f * horizontal * Time.deltaTime;
         position.y = position.y + 5.0f * vertical * Time.deltaTime;
-        transform.position = position;
+        Rigidbody2D.MovePosition(position);
     }
+
 }
+
